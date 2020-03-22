9b5f5a8ea510b2d6beeec85a724b8445a4279b23
rkevin <rk@rkevin.dev>
2020-03-21 23:02:41 -0700

    today we have learned that bbcode sucks

--- include/thirdparty/nbbc/nbbc.php
+++ include/thirdparty/nbbc/nbbc.php
@@ -680,7 +680,7 @@ class BBCodeLibrary
         ),
         'code' => Array(
             'mode' => BBCODE_MODE_ENHANCED,
-            'template' => "\n<div class=\"bbcode_code\">\n<div class=\"bbcode_code_head\">Code:</div>\n<div class=\"bbcode_code_body\" style=\"white-space:pre\">{\$_content/v}</div>\n</div>\n",
+            'template' => "<pre>{\$_content/v}</pre>",
             'class' => 'code',
             'allow_in' => Array('listitem', 'block', 'columns'),
             'content' => BBCODE_VERBATIM,
